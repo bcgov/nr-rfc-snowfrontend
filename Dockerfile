d4b11f23ce9885d0f6bd80ae903416a97191b046
Kevin Netherton <kevin.netherton@gov.bc.ca>
2023-03-30 11:22:09 -0700

    feat: creating dockerized version

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+#FROM python:alpine3.17
+FROM python:3.11-slim
+
+WORKDIR /app
+COPY ["src/constants.py", "src/logging.config", "src/data_interface.py", "src/main.py", "requirements.txt", "./"]
+
+RUN python -m pip install --upgrade pip && \
+    pip install -r ./requirements.txt && \
+    groupadd --gid 1000 appuser \
+    && useradd --uid 1000 --gid 1000 -ms /bin/bash appuser
+
+USER appuser
+
+#RUN git clone https://github.com/streamlit/streamlit-example.git app
+
+
+# RUN python -m pip install --upgrade pip && \
+#     pip install -r ./requirements.txt
+# ENV PATH="${PATH}:/home/appuser/.local/bin"
+
+EXPOSE 8501
+
+ENTRYPOINT ["streamlit", "run", "main.py"]
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import constants
 
 if 'logger' not in st.session_state:
 
-    log_config_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'config', 'logging.config'))
+    log_config_path = os.path.realpath(os.path.join(os.path.dirname(__file__), 'logging.config'))
     logging.config.fileConfig(log_config_path)
     LOGGER = logging.getLogger(__name__)
     st.session_state.logger = LOGGER
@@ -32,19 +32,32 @@ if 'wat_or_basin' not in st.session_state:
     st.session_state['wat_or_basin'] = constants.WAT_BASIN_OPTIONS[0]
     LOGGER.debug(f"setting wat_or_basin for first time: {st.session_state.wat_or_basin}")
 
-# TODO: change to select both by default
-if 'sat' not in st.session_state:
-    st.session_state['sat'] = constants.SAT_OPTIONS[0]
-    LOGGER.debug(f"setting the satellite type: {st.session_state.sat}")
-
 if 'area_name' not in st.session_state:
     st.session_state['area_name'] = "Boundary"
     LOGGER.debug(f"area name: {st.session_state.area_name}")
     #show_images(area_type=data_interface.AreaType['watersheds'], area_name='Boundary')
 
 
-def swap_area_type(*args, **kwargs):
-    pass
+def wat_basin_changed(*args, **kwargs):
+    LOGGER.debug(f"watbas - area type is: {st.session_state['wat_or_basin']}")
+    LOGGER.debug(f"watbas - satellites are {st.session_state['sat']}")
+    LOGGER.debug(f"watbas - area name: {st.session_state.area_name}")
+    # if the are type: (watershed/basin) changes then we need to
+    # 1) reload the basin / watershed names
+    # 2) choose default basin / watershed
+    # 3) reload the images
+    post_area_name_sidebar(st.session_state['wat_or_basin'])
+
+    #cur_sat = data_interface.Satellite[st.session_state['sat']]
+    area_type = data_interface.AreaType[st.session_state['wat_or_basin']]
+    name_url_dict = SPD.get_names(sat=cur_sat, area_type=area_type)
+
+
+    wat_basin = data_interface.AreaType[st.session_state.wat_or_basin.lower()]
+    show_images(area_type=wat_basin,
+                area_name=st.session_state.area_name,
+                sat_list=st.session_state.sat)
+
 
 def breakdown_type(*args, **kwargs):
     LOGGER.debug(f"area type is: {st.session_state['wat_or_basin']}")
@@ -53,95 +66,121 @@ def breakdown_type(*args, **kwargs):
 
     # convert the strings for satellite(sat) and area_type to enumerations
     wat_basin = data_interface.AreaType[st.session_state.wat_or_basin.lower()]
-    sat = data_interface.Satellite[st.session_state['sat'].lower()]
+
+    #sat = data_interface.Satellite[st.session_state['sat'][0].lower()]
 
     # get the dates that are available for the modis sat.. modis data is usually
     # more quickly processed than the viirs, and therefor the viirs is usually
     # a subset of modis... so just getting dates for modis then iterate
     # over those dates making the viirs data visiible if it can be found
-    show_images(area_type=wat_basin, area_name=st.session_state.area_name)
+    show_images(area_type=wat_basin,
+                area_name=st.session_state.area_name,
+                sat_list=st.session_state.sat)
 
-def show_images(area_type, area_name):
+def sat_changed(*args, **kwargs):
+    LOGGER.debug(f"sat - area type is: {st.session_state['wat_or_basin']}")
+    LOGGER.debug(f"sat - satellites are {st.session_state['sat']}")
+    if ('area_name' in st.session_state) and st.session_state.area_name:
+        # only update the images if the area type, and an actual area
+        # have been selected.
+        LOGGER.debug(f"sat - area name: {st.session_state.area_name}")
+
+        #names = list(sat_name_url_dict['modis'].keys())
+        name_url_dict = SPD.get_names(
+            sat=data_interface.Satellite['modis'], # area names are independent of satellite so just using modis here
+            area_type=data_interface.AreaType[st.session_state.wat_or_basin.lower()])
+        names = list(name_url_dict.keys())
+
+        wat_basin = data_interface.AreaType[st.session_state.wat_or_basin.lower()]
+
+        show_images(area_type=wat_basin,
+                    area_name=st.session_state.area_name,
+                    sat_list=st.session_state.sat)
+
+def show_images(area_type, area_name, sat_list):
+    # sat_list - list of sattellite data to display
     # determine what satellites are selected, use modis if its in the list
-    if 'modis' in st.session_state.sat:
-        cur_sat_str = 'modis'
+
+    LOGGER.debug(f"satlist: {sat_list}")
+    if not sat_list:
+        with DISPLAY.empty():
+            DISPLAY.write(f'## No Satellites are currently selected for viewing')
     else:
-        cur_sat_str = st.session_state.sat[0]
-    cur_sat = data_interface.Satellite[cur_sat_str]
+        if 'modis' in st.session_state.sat:
+            cur_sat_str = 'modis'
+        else:
+            cur_sat_str = sat_list[0]
+        cur_sat = data_interface.Satellite[cur_sat_str]
 
-    cur_sat_date_list = SPD.get_dates(
-        sat=cur_sat,
-        area_type=area_type,
-        number_of_dates=constants.DAYS_BACK
-        )
-    cur_sat_date_list.sort(reverse=True)
-    with DISPLAY.empty():
-        # Want to display the data for the given satellite / area type
-        # combination
-        # sooo
-        # ----
-        # Title: Area_name
-        # Date: date
-        # ----------------
-        # satellite: sat_name
-        # <image>
-        # satellite: sat_name
-        # <image>
-        #
-        # Date: date
-        # --------------
-        # satellite: sat_name
-        # <image>
-        # satellite: sat_name
-        # <image>
-        #
-        # ...blah blah blah
-        DISPLAY.write(f'# {area_type.name[0:-1].capitalize()}: *{area_name}* ')
-        for date in cur_sat_date_list:
-            #for sat_str in st.session_state.sat:
-            url_modis = SPD.get_url_by_date(
-                date_str=date,
-                sat=data_interface.Satellite['modis'],
-                area_type=area_type,
-                area_name=area_name)
-            url_viirs = SPD.get_url_by_date(
-                date_str=date,
-                sat=data_interface.Satellite['viirs'],
-                area_type=area_type,
-                area_name=area_name)
-            LOGGER.debug(f"modis url: - {url_modis}- {type(url_modis)}")
-            LOGGER.debug(f"viirs url: {url_viirs} {type(url_viirs)}")
+        cur_sat_date_list = SPD.get_dates(
+            sat=cur_sat,
+            area_type=area_type,
+            number_of_dates=constants.DAYS_BACK
+            )
+        cur_sat_date_list.sort(reverse=True)
+        with DISPLAY.empty():
+            # Want to display the data for the given satellite / area type
+            # combination
+            # sooo
+            # ----
+            # Title: Area_name
+            # Date: date
+            # ----------------
+            # satellite: sat_name
+            # <image>
+            # satellite: sat_name
+            # <image>
+            #
+            # Date: date
+            # --------------
+            # satellite: sat_name
+            # <image>
+            # satellite: sat_name
+            # <image>
+            #
+            # ...blah blah blah
+            DISPLAY.write(f'# {area_type.name[0:-1].capitalize()}: *{area_name}* ')
+            for date in cur_sat_date_list:
+                first_sat = True
+                LOGGER.debug(f"satellites: {sat_list} {type(sat_list)}")
+                for sat_str in sat_list:
+                    LOGGER.debug(f"sat_str: {sat_str}")
+                    if first_sat:
+                        first_sat = False
+                        DISPLAY.write(f'### Date: *{date}* ')
 
-            DISPLAY.write(f'### Date: *{date}* ')
-            DISPLAY.write(f'#### Satellite: *MODIS*')
-            DISPLAY.image(
-                    str(url_modis),
-                    width=600, # Manually Adjust the width of the image as per requirement
-                )
-            if url_viirs:
-                DISPLAY.write(f'#### Satellite: *VIIRS*')
-                DISPLAY.image(
-                        url_viirs,
-                        width=600, # Manually Adjust the width of the image as per requirement
-                    )
-            DISPLAY.write('---------')
+                    sat_url = SPD.get_url_by_date(
+                                date_str=date,
+                                sat=data_interface.Satellite['modis'],
+                                area_type=area_type,
+                                area_name=area_name)
+                    LOGGER.debug(f"{sat_str} url: - {sat_url}- {type(sat_url)}")
+                    DISPLAY.write(f'#### Satellite: *{sat_str}*')
+                    if sat_url:
+                        DISPLAY.image(
+                            str(sat_url),
+                            width=600, # Manually Adjust the width of the image as per requirement
+                        )
+                DISPLAY.write('---------')
 
 
 st.sidebar.success("Configure what you want to view.")
 
 # -- Satellite product to view
 # Add callbacks to change display when this is changed
-satellite = st.sidebar.multiselect(
+sat = st.sidebar.multiselect(
     label="Which Satelite based Snowpack Product",
     options=constants.SAT_OPTIONS,
-    default=constants.SAT_OPTIONS
+    default=constants.SAT_OPTIONS,
+    on_change=sat_changed,
+    key='sat'
 )
+LOGGER.debug(f"sat from multiselect: {sat}")
 
 # -- Watershed or Basin radio button
 wat_or_basin = st.sidebar.radio(
     "View data by Watershed or Basin",
-    on_change=breakdown_type,
-    #args=[st.session_state['wat_or_basin']],
+    on_change=wat_basin_changed,
     key='wat_or_basin',
     index=0,
     options=constants.WAT_BASIN_OPTIONS
@@ -152,35 +191,64 @@ DISPLAY.write("# Historical Snowpack Analysis ❄")
 # get the possible options for watershed / Basin
 # and populate into a select list
 LOGGER.debug(f"watershed or basin: {wat_or_basin}")
-LOGGER.debug(f"satellites: {satellite}")
+LOGGER.debug(f"satellites: {sat} {st.session_state.sat}")
+
+def post_area_name_sidebar(area_type_str):
+    global names
+    global sidebar_area_selector
+    LOGGER.debug(f"area type string: {area_type_str}")
+    area_type = data_interface.AreaType[area_type_str]
+    cur_sat = data_interface.Satellite['modis'] # always default
+
+    name_url_dict = SPD.get_names(
+        sat=cur_sat,
+        area_type=area_type)
+    names = list(name_url_dict.keys())
+
+    LOGGER.debug(f"watershed or basin: {wat_or_basin}")
+    LOGGER.debug(f"names: {names[0:4]}...")
+    LOGGER.debug(f"sidebar_area_selector: {sidebar_area_selector}")
+    with sidebar_area_selector.empty():
+        option = sidebar_area_selector.selectbox(
+            label=f'Which {wat_or_basin}',
+            options=names,
+            key='area_name',
+            on_change=breakdown_type,
+            index=0)
 
 # get the data
 sat_name_url_dict = {}
-for sat_str in satellite:
-    sat = data_interface.Satellite[sat_str]
+for sat_str in sat:
+    LOGGER.debug(f"sat_str: {sat_str}")
+    cur_sat = data_interface.Satellite[sat_str]
     area_type = data_interface.AreaType[wat_or_basin]
-    name_url_dict = SPD.get_names(sat=sat, area_type=area_type)
+    name_url_dict = SPD.get_names(sat=cur_sat, area_type=area_type)
     sat_name_url_dict[sat_str] = name_url_dict
 
-# TODO: come back and deal with multi sat
-names = list(sat_name_url_dict['modis'].keys())
 
-LOGGER.debug(f"names: {type(names)} {names}")
+sidebar_area_selector = st.sidebar.container()
 
-#st.spinner(text=f"loading {wat_or_basin} list...")
+LOGGER.debug(f"session state sat: {st.session_state.sat}")
+if st.session_state.sat:
+    # this is the default satellite - modis
+    # names = list(sat_name_url_dict['modis'].keys())
+    # LOGGER.debug(f"names: {type(names)} {names}")
 
-# populate the area pulldown
-option = st.sidebar.selectbox(
-    label=f'Which {wat_or_basin}',
-    options=names,
-    key='area_name',
-    on_change=breakdown_type,
-    index=0)
+    # # populate the area pulldown
+    # option = st.sidebar.selectbox(
+    #     label=f'Which {wat_or_basin}',
+    #     options=names,
+    #     key='area_name',
+    #     on_change=breakdown_type,
+    #     index=0)
+    post_area_name_sidebar(wat_or_basin)
 
 # show_images(area_type=data_interface.AreaType['watersheds'], area_name='Boundary')
 
 # # # load initial view
 if 'firstload' not in st.session_state:
     LOGGER.debug("loading first view")
-    show_images(area_type=data_interface.AreaType['watersheds'], area_name='Boundary')
+    show_images(area_type=data_interface.AreaType['watersheds'],
+                area_name='Boundary',
+                sat_list=st.session_state.sat)
     st.session_state.firstload = True
